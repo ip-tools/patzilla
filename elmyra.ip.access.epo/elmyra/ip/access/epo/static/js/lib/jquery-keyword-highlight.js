b273aad2f11bcb87f28a893253c42d53c7fe4308
Andreas Motl <andreas.motl@elmyra.de>
2014-05-20 20:26:14 +0200

    ui: reformat code

--- elmyra.ip.access.epo/elmyra/ip/access/epo/static/js/lib/jquery-keyword-highlight.js
+++ elmyra.ip.access.epo/elmyra/ip/access/epo/static/js/lib/jquery-keyword-highlight.js
@@ -14,9 +14,9 @@
                 var words = html.split(' ');
                 var new_content = '';
 
-                //check for inline data attributes.
+                // check for inline data attributes.
                 var currentKeyword = options.keyword;
-                if($(e).attr("data-keyword"))
+                if ($(e).attr("data-keyword"))
                     currentKeyword = $(e).attr("data-keyword");
 
                 if (currentKeyword == null) {
@@ -25,15 +25,15 @@
                 }
 
                 var currentHighlightClass = options.highlightClass;
-                if($(e).attr("data-highlightClass"))
+                if ($(e).attr("data-highlightClass"))
                     currentHighlightClass = $(e).attr("data-highlightClass");
 
                 var currentCaseSensitive = options.caseSensitive;
-                if($(e).attr("data-caseSensitive"))
+                if ($(e).attr("data-caseSensitive"))
                     currentCaseSensitive = $(e).attr("data-caseSensitive");
 
                 var currentContains = options.contains;
-                if($(e).attr("data-contains"))
+                if ($(e).attr("data-contains"))
                     currentContains = $(e).attr("data-contains");
 
                 $(words).each(function(i,e) {
@@ -45,26 +45,23 @@
                         }
 
                     } else if (currentCaseSensitive != 'true') {
-                        //not case sensitive, so lowercase all and compare.
-                        if(e.toLowerCase() == currentKeyword.toLowerCase())
+                        // not case sensitive, so lowercase all and compare.
+                        if (e.toLowerCase() == currentKeyword.toLowerCase())
                             found = true;
-                    }
-                    else
-                    {
-                        if(e == currentKeyword)
+                    } else {
+                        if (e == currentKeyword)
                             found = true;
                     }
 
-                    if(found) {
-                        //add span class around found word and add to new content
+                    if (found) {
+                        // add span class around found word and add to new content
                         new_content += '<span class="' + currentHighlightClass + '">' + e + '</span>' + ' ';
-                    }
-                    else
-                    {
+                    } else {
                         new_content += e  + ' ';
                     }
                 });
-                $(this).html(new_content); //place new content back into the targetted element
+                // place new content back into the targetted element
+                $(this).html(new_content);
             });
         }
     });
