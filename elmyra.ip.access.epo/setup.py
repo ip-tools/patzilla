49eb69b9c0f49d69c91d5b37a18359e824514585
Andreas Motl <andreas.motl@elmyra.de>
2014-06-15 08:46:56 +0200

    Bump version: 0.22.0 → 0.22.1

--- elmyra.ip.access.epo/setup.py
+++ elmyra.ip.access.epo/setup.py
@@ -71,7 +71,7 @@ requires = [
 ]
 
 setup(name='elmyra.ip.access.epo',
-      version='0.22.0',
+      version='0.22.1',
       description='elmyra.ip.access.epo',
       long_description=README,
       classifiers=[
