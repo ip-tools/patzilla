d18b27cf7c2681e33f404ecef132ebba0bc1cc85
Andreas Motl <andreas.motl@elmyra.de>
2014-07-12 03:08:37 +0200

    middleware/auth: clean up lua code formatting

--- lua/authentication.lua
+++ lua/authentication.lua
@@ -16,55 +16,56 @@ local users = config.users;
 headers = ngx.req.get_headers();
 
 function get_user()
-   local header = headers['Authorization']
-   if header == nil or header:find(" ") == nil then
-      return
-   end
+    local header = headers['Authorization']
+    if header == nil or header:find(" ") == nil then
+        return
+    end
 
-   local divider = header:find(' ')
-   if header:sub(0, divider-1) ~= 'Basic' then
-      return
-   end
+    local divider = header:find(' ')
+    if header:sub(0, divider-1) ~= 'Basic' then
+        return
+    end
 
-   local auth = ngx.decode_base64(header:sub(divider+1))
-   if auth == nil or auth:find(':') == nil then
-      return
-   end
+    local auth = ngx.decode_base64(header:sub(divider+1))
+    if auth == nil or auth:find(':') == nil then
+        return
+    end
 
-   divider = auth:find(':')
-   local user = auth:sub(0, divider-1)
-   local pass = auth:sub(divider+1)
-   if users[user] ~= pass then
-      return
-   end
+    divider = auth:find(':')
+    local user = auth:sub(0, divider-1)
+    local pass = auth:sub(divider+1)
+    if users[user] ~= pass then
+        return
+    end
 
-   return user
+    return user
 end
 
 function set_cookie()
-   local expires_after = 3600
-   local expiration = ngx.time() + expires_after
-   local token = expiration .. ":" .. ngx.encode_base64(ngx.hmac_sha1(
-       ngx.var.lua_auth_secret,
-       tostring(expiration)))
-   local cookie = "Auth=" .. token .. "; "
-   -- @TODO: Don't include subdomains
-   cookie = cookie .. "Path=/; Domain=" .. ngx.var.server_name .. "; "
-   cookie = cookie .. "Expires=" .. ngx.cookie_time(expiration) .. "; "
-   cookie = cookie .. "; Max-Age=" .. expires_after .. "; HttpOnly"
-   ngx.header['Set-Cookie'] = cookie
+    local expires_after = 3600
+    local expiration = ngx.time() + expires_after
+    local signature = ngx.encode_base64(ngx.hmac_sha1(ngx.var.lua_auth_secret, tostring(expiration)))
+    local token = expiration .. ":" .. signature
+    local cookie = "Auth=" .. token .. "; "
+    -- @TODO: Don't include subdomains
+    cookie = cookie .. "Path=/; "
+    -- cookie = cookie .. "Domain=" .. ngx.var.server_name .. "; "
+    cookie = cookie .. "Expires=" .. ngx.cookie_time(expiration) .. "; "
+    cookie = cookie .. "Max-Age=" .. expires_after .. "; "
+    cookie = cookie .. "HttpOnly"
+    ngx.header['Set-Cookie'] = cookie
 end
 
 local user = get_user()
 
 if user then
-   ngx.log(ngx.INFO, 'Authenticated user "' .. user .. '"')
-   set_cookie()
-   ngx.header.content_type = 'text/html'
-   ngx.say("<html><head><script>location.reload()</script></head></html>")
+    ngx.log(ngx.INFO, 'Authenticated user "' .. user .. '"')
+    set_cookie()
+    ngx.header.content_type = 'text/html'
+    ngx.say("<html><head><script>location.reload()</script></head></html>")
 else
-   ngx.header.content_type = 'text/plain'
-   ngx.header.www_authenticate = 'Basic realm=""'
-   ngx.status = ngx.HTTP_UNAUTHORIZED
-   ngx.say('401 Access Denied')
+    ngx.header.content_type = 'text/plain'
+    ngx.header.www_authenticate = 'Basic realm=""'
+    ngx.status = ngx.HTTP_UNAUTHORIZED
+    ngx.say('401 Access Denied')
 end
