96dbec567d49bfad97149b165a55df32641589f6
Andreas Motl <andreas.motl@elmyra.de>
2015-01-12 00:44:43 +0100

    middleware/ui: numberlist crawler for depatisnet

--- elmyra.ip.access.epo/elmyra/ip/access/epo/static/js/app/models/depatisnet.js
+++ elmyra.ip.access.epo/elmyra/ip/access/epo/static/js/app/models/depatisnet.js
@@ -5,6 +5,40 @@ DepatisnetSearch = DatasourceSearch.extend({
     url: '/api/depatisnet/published-data/search',
 });
 
+DepatisnetCrawler = Marionette.Controller.extend({
+
+    initialize: function(options) {
+        log('DepatisnetCrawler.initialize');
+        options = options || {};
+        this.query = options.query;
+        this.constituents = options.constituents;
+    },
+
+    crawl: function() {
+        var deferred = $.Deferred();
+        var url_tpl = _.template('/api/depatisnet/published-data/crawl/<%= constituents %>?query=<%= query %>');
+        var url = url_tpl({constituents: this.constituents, query: this.query});
+        var _this = this;
+        $.ajax({url: url, async: true})
+            .success(function(payload) {
+                if (payload) {
+                    if (_this.constituents == 'pub-number') {
+                        var numberlist = payload['numbers'];
+                        deferred.resolve(numberlist);
+                    } else {
+                        deferred.reject('Unknown constituents "' + _this.constituents + '"');
+                    }
+                } else {
+                    deferred.reject('Empty response');
+                }
+            }).error(function(error) {
+                deferred.reject('API failed', error);
+            });
+        return deferred;
+    },
+
+});
+
 DepatisConnectFulltext = Marionette.Controller.extend({
 
     initialize: function(document_number) {
