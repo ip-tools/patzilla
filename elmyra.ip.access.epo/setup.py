7bd9795bbc779106eb3428a9db193886f102adf3
Andreas Motl <andreas.motl@elmyra.de>
2015-05-18 12:09:59 +0200

    Bump version: 0.97.9 → 0.97.10

--- elmyra.ip.access.epo/setup.py
+++ elmyra.ip.access.epo/setup.py
@@ -96,7 +96,7 @@ test_requires = [
 ]
 
 setup(name='elmyra.ip.access.epo',
-      version='0.97.9',
+      version='0.97.10',
       description='elmyra.ip.access.epo',
       long_description=README,
       classifiers=[
