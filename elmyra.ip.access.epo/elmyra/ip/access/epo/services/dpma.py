cfd74733b9e4f17c2b1bbc0bcfb07867fcd4d8da
Andreas Motl <andreas.motl@elmyra.de>
2015-12-18 05:12:49 +0100

    DEPATISnet adapter: add feature “family-replace”

--- elmyra.ip.access.epo/elmyra/ip/access/epo/services/dpma.py
+++ elmyra.ip.access.epo/elmyra/ip/access/epo/services/dpma.py
@@ -137,9 +137,13 @@ def depatisnet_published_data_crawl_handler(request):
 
 
 def request_to_options(request, options):
+
     # TODO: transfer all modifiers 1:1
+
     if asbool(request.params.get('query_data[modifiers][family-remove]')):
         options.update({'feature_family_remove': True})
+    elif asbool(request.params.get('query_data[modifiers][family-replace]')):
+        options.update({'feature_family_replace': True})
 
     # this is awful, switch to JSON POST
     for key, value in request.params.iteritems():
