b54d99a68255450432b9852c36e0135bd8b9ac2a
Andreas Motl <andreas.motl@ip-tools.org>
2017-12-19 05:55:40 +0100

    Update setup.py, bump some dependency versions

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ requires = [
     'pyramid_debugtoolbar==4.3',
     'Pygments==2.2.0',
     'pyramid_mako==1.0.2',
-    'Jinja2==2.9.6',
+    'Jinja2==2.10',
     'Akhet==2.0',
     'waitress==1.1.0',
     'Paste==2.0.3',
@@ -32,15 +32,19 @@ requires = [
     # Caching
     'Beaker==1.9.0',
     'pyramid_beaker==0.8',
+    'dogpile.cache==0.6.4',
 
-    # Can't upgrade to pymongo-3.5.1 due to "from pymongo.connection import Connection" usage in "mongodb_gridfs_beaker" module.
-    'pymongo==2.9.5',           # 3.5.1
+    # Database
+    # Can't upgrade to pymongo-3.5.1 due to "from pymongo.connection import Connection"
+    # usage in "mongodb_gridfs_beaker" module.
+    'pymongo==2.9.5',           # 3.6.0
     'mongodb_gridfs_beaker==0.5.4',
+    'mongoengine==0.13.0',      # 0.15.0
 
-    # web services
-    'cornice==2.4.0',           # 2.4.1
+    # Web services
+    'cornice==2.4.1',
 
-    # authorization
+    # Authorization
     'pycrypto==2.6.1',
     'jws==0.1.3',
     'python_jwt==2.0.2',        # 3.0.0
@@ -48,7 +52,7 @@ requires = [
 
 
     # ----------------------------------------------
-    #   Infrastructure
+    #   Protocols and data formats
     # ----------------------------------------------
 
     # HTTP
@@ -56,44 +60,55 @@ requires = [
     'requests-oauthlib==0.8.0',
     'mechanize==0.3.6',
     'MechanicalSoup>=0.9.0.post4,<=1.1.0',
-    'dogpile.cache==0.6.4',
 
-    # HTML
-    'BeautifulSoup==3.2.1',     # beautifulsoup4==4.6.0
-
-    # SNI support
+    # HTTP SNI support
     'pyOpenSSL==17.3.0',
     'cryptography==2.0.3',      # 2.1.2
     'pyasn1==0.3.7',
     'ndg-httpsclient==0.4.3',
+
+    # HTML
+    'BeautifulSoup==3.2.1',     # beautifulsoup4==4.6.0
+    'html2text==2016.9.19',     # 2017.10.4
+
+    # XML
+    # Remark: Both lxml 3.8.0 and 4.0.0 will segfault on Debian Wheezy (7.11)
+    'lxml==3.4.1',              # 4.0.0
+
+    # JSON
+    'jsonpointer==1.14',
+
+
+    # ----------------------------------------------
+    #   Data handling and formatting
+    # ----------------------------------------------
+
+    # Data handling
+    'Bunch==1.0.1',
+    'pyparsing==2.0.2',         # 2.2.0
+    'python-dateutil==2.6.1',
+    'ago==0.0.9',
+    'arrow==0.10.0',
+    'validate_email==1.3',
+    'pandas==0.18.1',           # 0.20.3
+
+    # Data formatting
+    'openpyxl==2.1.0',          # 2.5.0a3
+    'xlrd==0.9.3',              # 1.1.0
+    'XlsxWriter==0.9.3',        # 1.0.0
+
+
+    # ----------------------------------------------
+    #   Program flow
+    # ----------------------------------------------
+    'blinker==1.4',
+    'transitions==0.2.4',       # 0.6.1
 
 
     # ----------------------------------------------
     #   Console interface
     # ----------------------------------------------
     'docopt==0.6.2',
-
-
-    # ----------------------------------------------
-    #   Business logic
-    # ----------------------------------------------
-    'Bunch==1.0.1',
-    'ago==0.0.9',
-    'pyparsing==2.0.2',         # 2.2.0
-    'mongoengine==0.13.0',      # 0.14.3
-    'blinker==1.4',
-    'python-dateutil==2.6.1',
-    # Remark: Both lxml 3.8.0 and 4.0.0 will segfault on Debian Wheezy (7.11)
-    'lxml==3.4.1',              # 4.0.0
-    'openpyxl==2.1.0',          # 2.5.0a3
-    'xlrd==0.9.3',              # 1.1.0
-    'XlsxWriter==0.9.3',        # 1.0.0
-    'jsonpointer==1.10',        # 1.12
-    'arrow==0.10.0',
-    'transitions==0.2.4',       # 0.6.1
-    'validate_email==1.3',
-    'pandas==0.18.1',           # 0.20.3
-    'html2text==2016.9.19',     # 2017.10.4
     'envoy==0.0.3',
 
 ]
