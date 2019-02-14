5dd0c9c96aa42be9f0c1663896c2fa585badf377
aghster <felix.berkemeier@gmx.de>
2019-02-14 15:19:22 +0100

    new service to fetch pdf from EPO publication server

---
+++ patzilla/navigator/services/publicationserver.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# (c) 2013-2018 Andreas Motl, Elmyra UG
+import logging
+from cornice.service import Service
+from pyramid.httpexceptions import HTTPNotFound
+from pyramid.settings import asbool
+from patzilla.access.epo.publicationserver.client import get_publication_server_pdf
+
+log = logging.getLogger(__name__)
+
+publicationserver_pdf_service = Service(
+    name='publicationserver-pdf',
+    path='/api/publicationserver/{patent}',
+    description="EPO publication server pdf interface")
+
+
+@publicationserver_pdf_service.get()
+def publicationserver_pdf_handler(request):
+    """request pdf from EPO publication server"""
+    # https://data.epo.org/publication-server/pdf-document?cc=EP&pn=nnnnnn&ki=nn
+
+    patent = request.matchdict['patent']
+
+    pdf_payload = get_publication_server_pdf(patent)
+
+    # http://tools.ietf.org/html/rfc6266#section-4.2
+    request.response.headers['Content-Disposition'] = 'inline; filename={0}.pdf'.format(patent)
+    request.response.headers['X-Pdf-Source'] = 'epo-publication-server'
+
+    return pdf_payload
