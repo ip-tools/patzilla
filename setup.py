cb2b0360e9e819ad6a0c1803bcae09c4f4a780e9
Andreas Motl <andreas.motl@ip-tools.org>
2019-02-14 13:46:30 +0100

    Bump version: 0.162.1 → 0.162.2

--- setup.py
+++ setup.py
@@ -131,7 +131,7 @@ test_requires = [
 ]
 
 setup(name='patzilla',
-      version='0.162.1',
+      version='0.162.2',
       description='PatZilla is a modular patent information research platform and data integration ' \
                   'toolkit. It features a modern user interface and access to multiple data sources.',
       long_description=README,
