03ebeef0cae2b2fb423fcc4804017eaeb3c796fd
FrankHeider <frank.heider@hasip.de>
2019-05-09 13:56:14 +0200

    Add python3_compat.py

---
+++ patzilla/util/python/python3_compat.py
@@ -0,0 +1,21 @@
+import sys
+
+_IS_PYTHON_3 = (sys.version_info[0] >= 3)
+
+identity = lambda x : x
+
+# u('string') replaces the forwards-incompatible u'string'
+if _IS_PYTHON_3:
+    u = identity
+else:
+    import codecs
+    def u(string):
+        return codecs.unicode_escape_decode(string)[0]
+
+# dict.iteritems(), dict.iterkeys() is also incompatible
+if _IS_PYTHON_3:
+    iteritems = dict.items
+    iterkeys  = dict.keys
+else:
+    iteritems = dict.iteritems
+    iterkeys = dict.iterkeys
