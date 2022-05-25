c3f1dfff54c815086c8c1b0e8f03a95421f5ada1
Andreas Motl <andreas.motl@ip-tools.org>
2022-05-25 02:04:42 +0200

    [mw] EPO/Espacenet: Repair access layer

--- patzilla/access/epo/espacenet/client.py
+++ patzilla/access/epo/espacenet/client.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
-# (c) 2015-2018 Andreas Motl <andreas.motl@ip-tools.org>
+# (c) 2015-2022 Andreas Motl <andreas.motl@ip-tools.org>
+"""
+Screenscraper for Espacenet
+https://worldwide.espacenet.com/
+"""
 import logging
+
 import requests
 from BeautifulSoup import BeautifulSoup
+
 from patzilla.util.network.browser import regular_user_agent
 from patzilla.util.numbers.normalize import normalize_patent
 
-
-"""
-Screenscraper for Espacenet
-http://worldwide.espacenet.com/
-"""
 
 logger = logging.getLogger(__name__)
 
@@ -18,10 +19,14 @@ def espacenet_fetch(document_number, section, element_id):
 
     patent = normalize_patent(document_number, as_dict=True, provider='espacenet')
 
-    # Blueprint: https://worldwide.espacenet.com/publicationDetails/biblio?CC=EP&NR=0666666&KC=A3
+    # Examples:
+    # - https://worldwide.espacenet.com/data/publicationDetails/biblio?CC=EP&NR=0666666A3&KC=A3&FT=D
+    # - https://worldwide.espacenet.com/data/publicationDetails/claims?CC=EP&NR=0666666B1&KC=B1&FT=D
     url_tpl = u'https://worldwide.espacenet.com/data/publicationDetails/{section}?CC={country}&NR={number}'
     if 'kind' in patent and patent['kind']:
+        patent["number"] += patent["kind"]
         url_tpl += '&KC={kind}'
+    url_tpl += "&FT=D"
 
     url = url_tpl.format(section=section, **patent)
 
@@ -68,8 +73,8 @@ def espacenet_fetch(document_number, section, element_id):
 def espacenet_description(document_number):
     """
     Return Espacenet description fulltext
-    http://worldwide.espacenet.com/publicationDetails/description?CC=US&NR=5770123A&DB=worldwide.espacenet.com&FT=D
-    http://worldwide.espacenet.com/publicationDetails/description?CC=DE&NR=19814298A1&DB=worldwide.espacenet.com&FT=D
+    https://worldwide.espacenet.com/data/publicationDetails/description?CC=US&NR=5770123A&DB=worldwide.espacenet.com&FT=D
+    https://worldwide.espacenet.com/data/publicationDetails/description?CC=DE&NR=19814298A1&DB=worldwide.espacenet.com&FT=D
     """
     return espacenet_fetch(document_number, 'description', 'description')
 
@@ -77,15 +82,28 @@ def espacenet_description(document_number):
 def espacenet_claims(document_number):
     """
     Return Espacenet claims fulltext
-    http://worldwide.espacenet.com/publicationDetails/claims?CC=US&NR=5770123A&FT=D&DB=worldwide.espacenet.com
-    http://worldwide.espacenet.com/publicationDetails/claims?CC=DE&NR=19814298A1&DB=worldwide.espacenet.com&FT=D
+    https://worldwide.espacenet.com/data/publicationDetails/claims?CC=US&NR=5770123A&FT=D&DB=worldwide.espacenet.com
+    https://worldwide.espacenet.com/data/publicationDetails/claims?CC=DE&NR=19814298A1&DB=worldwide.espacenet.com&FT=D
     """
     return espacenet_fetch(document_number, 'claims', 'claims')
 
 
 if __name__ == '__main__':
-    #print espacenet_description('DE19814298A1')
-    #print espacenet_description('US5770123A')
-
-    #print espacenet_claims('DE19814298A1')
-    print espacenet_claims('US5770123A')
+    """
+    python -m patzilla.access.epo.espacenet.client
+    """
+    numbers = [
+        "US5770123A",
+        "US6269530B1",
+        "DE19814298A1",
+        "DE29624638U1",
+    ]
+    for number in numbers:
+        print("## {}".format(number))
+        print("")
+        print("### Claims")
+        print(espacenet_claims(number))
+        print("")
+        print("### Description")
+        print(espacenet_description(number))
+        print("\n")
