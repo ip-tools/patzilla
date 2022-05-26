bc10015c6185c236bfd0e9d2f38d26dea2328ed3
Andreas Motl <andreas.motl@ip-tools.org>
2022-05-26 02:35:16 +0200

    [mw] EPO/Espacenet: Acquire "abstract" text

--- tests/access/test_epo_espacenet.py
+++ tests/access/test_epo_espacenet.py
@@ -2,7 +2,26 @@
 # (c) 2022 Andreas Motl <andreas.motl@ip-tools.org>
 import pytest
 
-from patzilla.access.epo.espacenet.client import espacenet_description, espacenet_claims
+from patzilla.access.epo.espacenet.client import espacenet_description, espacenet_claims, espacenet_abstract
+
+
+def test_espacenet_abstract_success(app_request):
+    """
+    Acquire "abstract" section of valid patent document from Espacenet.
+    """
+    result = espacenet_abstract(document_number="EP0666666B1")
+    assert result["source"] == "espacenet"
+    assert result["lang"] == "en"
+    assert "A non-quota access indicator is circulated among nodes" in result["xml"]
+
+
+def test_espacenet_abstract_failure(app_request):
+    """
+    Acquire "abstract" section of invalid patent document from Espacenet.
+    """
+    with pytest.raises(KeyError) as ex:
+        espacenet_abstract(document_number="EP123A2")
+    assert ex.match('No section "biblio" at Espacenet for "EP123A2"')
 
 
 def test_espacenet_description_success(app_request):
