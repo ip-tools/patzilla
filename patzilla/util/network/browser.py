10e15e8e9c46a853fb47e55b962aad3496724fa3
Andreas Motl <andreas.motl@ip-tools.org>
2022-06-03 04:16:44 +0200

    [mw] Fix access to DPMAregister due to upstream changes
    Upstream changes needed some adjustments.
    
    > DPMAregister will have a new layout from 1 June 2022.
    > The pages have been streamlined.
    >
    > -- https://register.dpma.de/DPMAregister/service/aktuelles#a57

--- patzilla/util/network/browser.py
+++ patzilla/util/network/browser.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 # (c) 2017-2019 Andreas Motl <andreas.motl@ip-tools.org>
 
-regular_user_agent = u'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:100.0) Gecko/20100101 Firefox/100.0'
+regular_user_agent = u'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0'
